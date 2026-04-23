9b6f77bf5c561e13e2f1cb5401d2fd7bd980327e
najibullah-platforms <najibullah.platforms@gmail.com>
2026-04-23 20:49:38 +0500

    whatsapp added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react'
-import {About , Footer, Header, Skills , Testimonial, Work} from './container'
+import { About, Footer, Header, Skills, Testimonial, Work } from './container'
 import { Navbar } from './components'
 import './App.scss'
+import './style.css'
+
 const App = () => {
   return (
-    <div className='app'>
-    <Navbar />
-     <Header />
-     <About />
-     <Work />
-     <Skills />
-     <Testimonial />
-     <Footer />
-    </div>
+    <>
+      <div className='app'>
+        <Navbar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Testimonial />
+        <Footer />
+      </div>
+
+      <div className="whatsapp-btn-container">
+        <a 
+          className="whatsapp-btn" 
+          href="https://wa.me/91999999999"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-whatsapp"></i>
+        </a>
+        <span>Contact Us</span>
+      </div>
+    </>
   )
 }
 
